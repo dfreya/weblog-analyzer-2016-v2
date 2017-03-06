a7a47f6e87e91c42cb2569b3b8c3a3759704be00
dfreya <dbrja.dam@gmail.com>
2017-03-06 12:11:21 +0100

    TestRama0543aCommit01 OK

--- Acceso.java
+++ Acceso.java
@@ -6,13 +6,20 @@ public class Acceso
     private int hora;
     private int minutos;
     
-    public Acceso(int ano, int mes, int dia, int hora, int minutos)
+    public Acceso(String logEntrada)
     {
-        this.ano = ano;
-        this.mes = mes;
-        this.dia = dia;
-        this.hora = hora;
-        this.minutos = minutos;
+        
+        
+        String fechaYHora[] = logEntrada.split(" ");
+       
+        
+        ano = Integer.parseInt(fechaYHora[0]);
+        mes = Integer.parseInt(fechaYHora[1]);
+        dia = Integer.parseInt(fechaYHora[2]);
+        hora = Integer.parseInt(fechaYHora[3]);
+        minutos = Integer.parseInt(fechaYHora[4]);
+       
+        
     }
     
     public int getAno() 
